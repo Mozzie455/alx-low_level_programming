6410908468da0a4873ac69dded24c8ddf92379a9
root <root@Mozzie455.localdomain>
2022-03-15 15:56:01 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char output[] = "_putchar";
+
+	while (i < 9)
+	{
+		fputchar(output[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
