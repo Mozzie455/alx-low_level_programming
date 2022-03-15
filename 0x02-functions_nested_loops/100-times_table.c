9027d4a5922de1fc7fb374df96d07cc2007b82f0
Mozzie <joshuamutiso455@gmail.com>
2022-03-15 23:01:44 +0300

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,36 +12,40 @@ void print_times_table(int n)
 {
 	int a, b, op;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15 || n < 0)
 	{
-	for (a = 0; a <= n; a++)
-	{
-	_putchar(48);
-	for (b = 1; b <= n; b++)
-	{
-		op = a * b;
-		_putchar(44);
-		_putchar(32);
-		if (op <= 9)
+		for (a = 0; a <= n; a++)
 		{
-		_putchar(32);
-		_putchar(32);
-		_putchar(op + 48)
+			for (b = 0; b <= n; b++)
+			{
+				op = a * b;
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (op <= 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((op % 10) + '0');
+				}
+				else if (op > -10 && op < 100)
+				{
+					_putchar(' ');
+					_putchar((op / 10) + '0');
+					_putchar((op % 10) + '0');
+				}
+				else if (op > 100 && j != 0)
+				{
+					_putchar((op / 100) + '0');
+					_putchar((op / 10) % 10 + '0');
+					_putchar((op % 10) + '0');
+				}
+				else
+					 _putchar((op % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		else if (op <= 99)
-		{
-		_putchar(32);
-		_putchar((op / 10) + 48);
-		_putchar((op / 10) + 48);
-		}
-		else
-		{
-		_putchar(((op / 100) % 10) + 48);
-		_putchar(((op / 10) % 10) + 48);
-		_putchar(((op % 10) + 48);
-		}
-	}
-	_putchar('\n');
-	}
 	}
 }
