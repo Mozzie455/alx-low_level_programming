ed92c75d3bb576c001f8d7602e18b6e35403c8a5
Joshua <joshuamutiso455@gmail.com>
2022-03-29 20:42:45 +0300

    print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,23 @@
-#include "holberton.h"
+# include "main.h"
 
 /**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
+ * print_chessboard - prints the chessboard
+ * @a: the chessboard
+ *
+ * Return: void
  */
+
 void print_chessboard(char (*a)[8])
 {
-	int i, n;
+	int x;
+	int y;
 
-	for (i = 0; i < 8; i++)
+	for (x = 0; x < 8; x++)
 	{
-		for (n = 0; n < 8; n++)
-			_putchar(a[i][n]);
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
 		_putchar('\n');
 	}
 }
