b1474554440965fd514cfa560fc5b92825bcb5aa
Mozzie455 <joshuamutiso455@gamil.com>
2022-06-09 21:44:12 +0300

    Print list

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef DOUBLY_LIST
+#define DOUBLY_LIST
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton projec
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+size_t print_dlistint(const dlistint_t *h);
+#endif
