794db053dff0172a0992288c157b676cd5a2a507
Mozzie455 <joshuamutiso455@gamil.com>
2022-07-01 12:12:12 +0300

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,31 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_create - creates a hash table.
-  * @size: sizeof the hash table.
-  *
-  * Return: pointer to has table type.
-  */
+ * hash_table_create - create a new hash table
+ * @size: size of array with hash table
+ *
+ * Return: pointer to new hash table
+ * and return NULL if fail
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	if (!size)
+	if (size < 1)
 		return (NULL);
 
-	ht = calloc(1, sizeof(hash_table_t));
-	if (!ht)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	ht->size = size;
+
 	ht->array = calloc(size, sizeof(hash_node_t *));
-	if (!ht->array)
+	ht->size = size;
+
+	if ((ht->array) == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
+
 	return (ht);
 }
